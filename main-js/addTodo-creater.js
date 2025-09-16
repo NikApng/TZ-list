f1f0b3bd1a0d088b08c586b37c09166fc5edae08
NikApng <krasilnikovnikita11@gmail.com>
2025-09-16 22:34:04 +0300

    update a task list and with a focus on the next stage

--- main-js/addTodo-creater.js
+++
@@ -1,8 +0,0 @@
-function Editer(a, b) {
-    let result = 1
-
-    for (let i = 0; i < b; b++){
-        result *=a
-        return result
-    }
-}
--- main-js/stogare.js
+++ main-js/stogare.js
@@ -1,180 +1,187 @@
-// function saveToLocalStorage(key, value) {
-//   localStorage.setItem(key, JSON.stringify(value))
-// }
+function saveToLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+}
 
-// function getToLocalStorage(key) {
-//   const data = localStorage.getItem(key)
-//   return data ? JSON.parse(data) : null
-// }
-
-// function RenderTodofromLocalStorage() {
-
-//   const listUL = document.querySelector('[data-ul-list]')
-//   if (!listUL) return
-
-//   listUL.innerHTML = ''
+function getToLocalStorage(key) {
+  const data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : null
+}
 
 
-//   // localStorage.removeItem('listUL')
+function RenderTodofromLocalStorage () {
+  const listUL = document.querySelector('[data-ul-list]')
+  if (!listUL) return
+
+  listUL.innerHTML = ''
+  let tasks = getToLocalStorage('listUL') || []
+
+  tasks.forEach((task) =>{
+    const taska = document.createElement('li')
+    taska.classList.add('lefSide--LI')
+
+    const inputReady = document.createElement('input')
+    inputReady.type = 'checkbox'
+    inputReady.classList.add('lefSide--inputReady')
+
+    const taskaText = document.createElement('span')
+    taskaText.textContent = task.text
+    taskaText.classList.add('lefSide--task')
+    
+
+    const taskaDeleteBtn = document.createElement('button')
+taskaDeleteBtn.classList.add('lefSide--taskaBtn')
+
+    taskaDeleteBtn.textContent = '🗑️'
 
 
+    const taskaEditBtn = document.createElement('button')
+    taskaEditBtn.classList.add('lefSide--taskaBtn')
+    
 
-//   let tasks = getToLocalStorage('listUL') || []
+    taskaEditBtn.textContent = '📝'
 
-//   tasks.forEach(task => {
+    inputReady.checked = task.done
+    if (task.done === true) {
+      taskaText.style.color = 'gray'
+      taskaText.style.textDecoration = 'line-through'
 
-//     const taska = document.createElement('li')
-//     taska.style.borderBottom = '1px black solid'
-//     taska.style.margin = '5%'
-
-//     const inputReady = document.createElement('input')
-//     inputReady.type = 'checkbox'
-
-
-
-
-//     const taskaText = document.createElement('span')
-//     taskaText.textContent = task.text
-
-//     const taskaDeleteBtn = document.createElement('button')
-//     taskaDeleteBtn.style.marginLeft = '10px'
-//     taskaDeleteBtn.style.border = 'dashed'
-//     taskaDeleteBtn.style.borderRadius = '1rem'
-//     taskaDeleteBtn.style.background = 'transparent'
-//     taskaDeleteBtn.style.cursor = 'pointer'
-
-//     taskaDeleteBtn.textContent = '🗑️'
+    } else {
+      taskaText.style.color = 'white'
+      taskaText.style.fontWeight = 'bold'
+      taskaText.style.textDecoration = 'none'
+    }
 
 
-//     const taskaEditBtn = document.createElement('button')
-//     taskaEditBtn.style.marginLeft = '10px'
-//     taskaEditBtn.style.border = 'dashed'
-//     taskaEditBtn.style.borderRadius = '1rem'
-//     taskaEditBtn.style.background = 'transparent'
-//     taskaEditBtn.style.cursor = 'pointer'
-//     taskaEditBtn.style.textAlign = 'center'
-
-//     taskaEditBtn.textContent = '📝'
-
-//     inputReady.checked = task.done
-//     if (task.done === true) {
-//       taskaText.style.color = 'gray'
-//       taskaText.style.textDecoration = 'line-through'
-
-//     } else {
-//       taskaText.style.color = 'white'
-//       taskaText.style.fontWeight = 'bold'
-//       taskaText.style.textDecoration = 'none'
-//     }
-
-
-//     listUL.appendChild(taska)
-//     taska.appendChild(inputReady)
-//     taska.appendChild(taskaText)
-//     taska.appendChild(taskaEditBtn)
-//     taska.appendChild(taskaDeleteBtn)
-//     const btnDeleteAll = document.querySelector('[data-delete-all-btn]')
-//     btnDeleteAll.textContent = 'очистить список'
-
-
-//     btnDeleteAll.addEventListener('click', () => {
-
-//       localStorage.removeItem('listUL')
-
-//       RenderTodofromLocalStorage()
-//     })
-
-//     taskaDeleteBtn.addEventListener('click', (el) => {
-
-//       let tasks = getToLocalStorage('listUL') || []
-//       taska.remove()
-//       tasks = tasks.filter(t => t.id !== task.id)
-//       saveToLocalStorage('listUL', tasks)
-//       RenderTodofromLocalStorage()
-
-//     })
-
-//     inputReady.addEventListener('change', () => {
-//       let tasks = getToLocalStorage('listUL') || []
-//       tasks = tasks.map(t =>
-//         t.id === task.id ? { ...t, done: inputReady.checked } : t
-//       )
-//       saveToLocalStorage('listUL', tasks)
-//       if (inputReady.checked) {
-//         taskaText.style.color = 'gray'
-//         taskaText.style.textDecoration = 'line-through'
-//       } else {
-//         taskaText.style.color = 'white'
-//         taskaText.style.fontWeight = 'bold'
-//         taskaText.style.textDecoration = 'none'
-//       }
-//     })
+    listUL.appendChild(taska)
+    taska.appendChild(inputReady)
+    taska.appendChild(taskaText)
+    taska.appendChild(taskaEditBtn)
+    taska.appendChild(taskaDeleteBtn)
+    const btnDeleteAll = document.querySelector('[data-delete-all-btn]')
 
 
 
-//   })
-// }
-// RenderTodofromLocalStorage()
+    btnDeleteAll.addEventListener('click', () => {
 
-// document.addEventListener('click', (e) => {
-//   const input = document.querySelector('[data-name-input]')
+      localStorage.removeItem('listUL')
 
-//   const listUL = document.querySelector('[data-ul-list]')
+      RenderTodofromLocalStorage()
+    })
 
-//   if (e.target.closest('[data-create-btn]')) {
+    taskaDeleteBtn.addEventListener('click', (el) => {
 
-//     const task = document.createElement('li')
+      let tasks = getToLocalStorage('listUL') || []
+      taska.remove()
+      tasks = tasks.filter(t => t.id !== task.id)
+      saveToLocalStorage('listUL', tasks)
+      RenderTodofromLocalStorage()
 
-//     const ids = Date.now()
-//     const text = document.createElement('span')
+    })
 
-//     const deleteBtn = document.createElement('button')
-
-
-//     const editBtn = document.createElement('button')
-
-//     editBtn.textContent = '/'
-//     deleteBtn.textContent = '🗑️'
-
-
-//     text.textContent = input.value.trim()
-//     if (!input.value.trim()) return
-
-//     listUL.appendChild(task)
-//     task.appendChild(text)
-//     task.appendChild(editBtn)
-//     task.appendChild(deleteBtn)
+    inputReady.addEventListener('change', () => {
+      let tasks = getToLocalStorage('listUL') || []
+      tasks = tasks.map(t =>
+        t.id === task.id ? { ...t, done: inputReady.checked } : t
+      )
+      saveToLocalStorage('listUL', tasks)
+      if (inputReady.checked) {
+        taskaText.style.color = 'gray'
+        taskaText.style.textDecoration = 'line-through'
+      } else {
+        taskaText.style.color = 'white'
+        taskaText.style.fontWeight = 'bold'
+        taskaText.style.textDecoration = 'none'
+      }
+    })
 
 
-//     let tasks = getToLocalStorage('listUL') || []
-//     tasks.push({
-//       id: ids,
-//       text: input.value.trim(),
-//       done: false
-//     })
-//     saveToLocalStorage('listUL', tasks)
-//     RenderTodofromLocalStorage()
 
-//     input.value = ''
-//     input.focus()
-//   }
-// })
+  })
+  
+}
+RenderTodofromLocalStorage()
+
+
+
+
 
 
 document.addEventListener('click', (e)=>{
 
   if(e.target.closest('[data-create-btn]')){
-      
+    const listUL = document.querySelector('[data-ul-list]')
     const menuAdder = document.createElement('div')
 
+    const id = Date.now()
+
     menuAdder.innerHTML = `
-    <div class="new">
-    <h3>Создание задачи</h3>
-    </div>
-`
+                    <div class="Menu-adder__container">
+                        <div class="Menu-adder__header">
+                            <button class="Menu-exit__menu--btn" data-remove type="button">✚</button>
+
+                            <h3 class="Menu-text__header">Создание задачи</h3> 
+                            
+                        </div>
+                            <div class="Menu-bottom__container">
+                                <input type="text" class="Menu-input" data-input-menu="${id}">
+                                <button class="Menu-add__task--btn" data-add-task-in-menu="${id}" type="button">✚</button>
+                            </div>
+                            
+                    </div>
+    `
+    const input = menuAdder.querySelector(`[data-input-menu="${id}"]`)
+    const addBtn = menuAdder.querySelector(`[data-add-task-in-menu="${id}"]`)
+
+
+    addBtn.addEventListener('click', ()=>{
+    const valueInp = input.value.trim()
+    const task = document.createElement('li')
+    
+
+    const text = document.createElement('span')
+    text.classList.add = ('LeftSide--task')
+
+    const deleteBtn = document.createElement('button')
+
+    const editBtn = document.createElement('button')
+
+    editBtn.textContent = '/'
+    deleteBtn.textContent = '🗑️'
+
+
+    text.textContent = input.value.trim()
+    if (!input.value.trim()) return
+
+    listUL.appendChild(task)
+    task.appendChild(text)
+    task.appendChild(editBtn)
+    task.appendChild(deleteBtn)
+      
+
+    input.value = ''
+    input.focus()
+
+      let tasks = getToLocalStorage('listUL') || []
+    tasks.push({
+      id: id,
+      text: valueInp,
+      done: false
+    })
+    saveToLocalStorage('listUL', tasks)
+    RenderTodofromLocalStorage()
+    })
+
+    const valueInp = input.value.trim()
+
+
     const rightMenu = document.querySelector('[data-right-menu]')
     rightMenu.appendChild(menuAdder)
 
+const btnRemove = document.querySelector('[data-remove]')
+btnRemove.addEventListener('click', ()=>{
+menuAdder.remove()
+})
+
 }
-    
+
 })
